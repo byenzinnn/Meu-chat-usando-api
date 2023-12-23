b4e287998ff02197d1fee93994c326f6570e534d
byenzinnn <enzodearaujogrellmann@hotmail.com>
2023-12-23 14:04:51 -0300

    Algumas fontes alteradas

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -2,12 +2,13 @@ import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const ChatsPage = (props) => {
     return (
+        
         <div style={{height: '100vh'}}>
             <PrettyChatWindow
             projectId='6781a6fa-c039-43df-ab74-1ab10b553ea0'
             username={props.user.username}
             secret={props.user.username}
-            style={{ height: '100%'}}
+            style={{ height: '100%' }}
             />
         </div>
     )
